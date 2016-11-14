c46a15a73f7891a693feabbbabb522e8349b4ecf
Colin <craymond@Colins-MacBook-Pro.local>
2016-11-13 22:27:39 -0500

    Ack

--- js/app.js
+++ js/app.js
@@ -28,13 +28,15 @@ NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
   var narrow = this;
   narrow.searchTerm = "";
-  //narrow.found = [{name:}];
 
-  narrow.getMatchedMenuItems = function(){
-    narrow.found = MenuSearchService.getMatchedMenuItems(narrow.searchTerm);
-    narrow.found = MenuSearchService.testMatch();
-    console.log(narrow.found[1]);
-  };
+  narrow.getMatchedMenuItems = MenuSearchService.getMatchedMenuItems(narrow.searchTerm);
+
+  promise.then(function (response) {
+    narrow.found = response.data;
+  })
+  .catch(function (error) {
+    console.log('Uh oh!');
+  });
 
   // found.removeItem = function(index){
   //   console.log('Removed: ' + found.meals[index].name + ' Items left: ' + found.meals.length);
@@ -46,12 +48,6 @@ MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 function MenuSearchService($http, ApiBasePath){
   var service = this;
 
-  service.testMatch = function(){
-    var foundItems = ['Test','TestTwo'];
-
-    return foundItems;
-  }
-
   service.getMatchedMenuItems = function(searchTerm){
     var foundItems = [];
 
@@ -60,23 +56,25 @@ function MenuSearchService($http, ApiBasePath){
       var foundItems = [];
 
       for(var i = 0; i < result.data.menu_items.length; i++){
-        // if(result.data.menu_items[i].description.includes(searchTerm)){
-        //   var item = {
-        //     name: result.data.menu_items[i].name,
-        //     description: result.data.menu_items[i].description
-        //   };
-
-        var item = {
-          name: result.data.menu_items[i].name,
-          description: result.data.menu_items[i].description
-        };
+        if(result.data.menu_items[i].description.includes(searchTerm)){
+          var item = {
+            name: result.data.menu_items[i].name,
+            short_name: result.data.menu_items[i].short_name,
+            description: result.data.menu_items[i].description
+          }
+        } else {
+          var item = {
+            name: result.data.menu_items[i].name,
+            short_name: result.data.menu_items[i].short_name,
+            description: result.data.menu_items[i].description
+          };
+        }
 
         foundItems.push(item);
       };
 
-      var temp_array = ["TEST","TEST2"];
-      console.log(temp_array[0]);
-      return temp_array;
+      console.log(foundItems);
+      return foundItems;
 
     })
     .catch(function (errorResponse) {
